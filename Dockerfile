4c956efd73a68b4eb817d434d09e5ba5bb61b713
c3h3 <c3h3.tw@gmail.com>
2015-03-29 22:34:27 +0800

    init Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM c3h3/rstudio-java:latest
+
+MAINTAINER Chia-Chi Chang <c3h3.tw@gmail.com>
+
+RUN mkdir /demo
+ADD package_installer.R /demo/package_installer.R
+RUN cd /demo && Rscript package_installer.R
+
+ADD userconf.sh /usr/bin/userconf.sh
+RUN chmod a+x /usr/bin/userconf.sh
+
---
+++ package_installer.R
@@ -0,0 +1,11 @@
+
+# http://rstudio-pubs-static.s3.amazonaws.com/12422_b2b48bb2da7942acaca5ace45bd8c60c.html
+
+install.packages("rJava")
+install.packages("Rwordseg", repos="http://R-Forge.R-project.org")
+install.packages("tm")
+install.packages("tmcn", repos="http://R-Forge.R-project.org", type="source")
+install.packages("wordcloud")
+install.packages("XML")
+install.packages("RCurl")
+
